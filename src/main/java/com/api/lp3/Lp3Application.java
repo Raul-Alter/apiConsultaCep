90a7e147a1e83ba894558468ebc69a6620fb051d
Raul-Alter <nissemonog@hotmail.com>
2024-03-02 01:54:53 -0300

    Criação do Projeto

---
+++ src/main/java/com/api/lp3/Lp3Application.java
@@ -0,0 +1,13 @@
+package com.api.lp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/lp3/Lp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.lp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
